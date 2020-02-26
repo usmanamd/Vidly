780498cc5f1b2eac8bdc86466f7a79ac6f1e77fd
usmanamd <usmanamd@gmail.com>
2020-02-26 21:49:44 +0000

    added the environment files

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.min.css";
 
+console.log(process.env)
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
